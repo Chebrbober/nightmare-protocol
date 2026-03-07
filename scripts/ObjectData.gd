9426deed0ba2c5a754a5ce864e345dde8051df8e
beaver <meyar2223@gmail.com>
2026-03-07 14:07:59 +0200

    ObjectData script that handles object's properties

---
+++ scripts/ObjectData.gd
@@ -0,0 +1,8 @@
+class_name ObjectData
+extends Resource
+
+@export var name: String
+@export var description: String
+@export var type: String
+@export var texture: Texture2D
+@export var shape: Shape2D
